32092059b925157ec187d7fe0d3060de3575b9c2
pablobvch <pablobvch@gmail.com>
2020-04-14 20:19:00 -0300

    feat: integrate error handler

--- pages/Channel_old.jsx
+++ pages/Channel_old.jsx
@@ -69,50 +69,60 @@ const Channel = ({ channel, audioClips, childChannels, statusCode }) =>
     ? renderChannel(channel, audioClips, childChannels)
     : renderErrorMessage(statusCode);
 
+/*Channel.getInitialProps = async ({ query }) => {
+  let reqChannel = await fetch(
+    `https://api.audioboom.com/channels/${query.id}`
+  );
+  let dataChannel = await reqChannel.json();
+  let channel = dataChannel.body.channel;
+
+  let reqAudio = await fetch(
+    `https://api.audioboom.com/channels/${query.id}/audio_clips`
+  );
+  let dataAudio = await reqAudio.json();
+  let audio_clips = dataAudio.body.audio_clips;
+
+  return { channel, audio_clips };
+};*/
+
 const renameBodytoChildChannels = (body) => body.channels;
 
 const renameBodytoAudioClips = (body) => body.audio_clips;
 
 const renameBodytoChannel = (body) => body.channel;
 
-Channel.getInitialProps = async ({ query, res }) => {
-  let idChannel = query.id;
-
-  try {
-    let [reqChannel, reqChildChannels, reqAudioClips] = await Promise.all([
-      fetch(`https://api.audioboom.com/channels/${idChannel}`),
-      fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
-      fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`),
-    ]);
-
-    if (reqChannel.status >= 400) {
-      res.statusCode = reqChannel.status;
+Channel.getInitialProps = ({ query, res }) =>
+  /*Promise.all([
+    fetch(`https://api.audioboom.com/channels/${query.id}`)
+      .then((response) => {
+        if (response.status >= 400) {
+          res.statusCode = response.status;
+          throw new Exception();
+        }
+        return response.json();
+      })
+      .then((response) => renameBodytoChannel(response.body)),
+    fetch(`https://api.audioboom.com/channels/${query.id}/audio_clips`)
+      .then((response) => response.json())
+      .then((response) => renameBodytoAudioClips(response.body)),
+    fetch(`https://api.audioboom.com/channels/${query.id}/child_channels`)
+      .then((response) => response.json())
+      .then((response) => renameBodytoChildChannels(response.body)),
+  ])
+    .then(([channel, audioClips, childChannels]) => ({
+      channel,
+      audioClips,
+      childChannels,
+      statusCode: 200,
+    }))
+    .catch((e) => {
+      //console.log("res.statusCode", res.statusCode);
       return {
         channel: null,
         audioClips: null,
         childChannels: null,
-        statusCode: reqChannel.status,
+        statusCode: res.statusCode || 503,
       };
-    }
-
-    let dataChannel = await reqChannel.json();
-    let channel = dataChannel.body.channel;
-
-    let dataAudioClips = await reqAudioClips.json();
-    let audioClips = dataAudioClips.body.audio_clips;
-
-    let dataChildChannels = await reqChildChannels.json();
-    let childChannels = dataChildChannels.body.channels;
-
-    return { channel, audioClips, childChannels, statusCode: 200 };
-  } catch (e) {
-    return {
-      channel: null,
-      audioClips: null,
-      childChannels: null,
-      statusCode: 503,
-    };
-  }
-};
+    });*/
 
 export default Channel;
